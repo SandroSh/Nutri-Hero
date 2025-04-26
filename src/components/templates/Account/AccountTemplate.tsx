74059373a644480157840c3458cfc5c8940aeda0
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2025-04-27 02:15:01 +0400

    fix error

--- src/components/templates/Account/AccountTemplate.tsx
+++ src/components/templates/Account/AccountTemplate.tsx
@@ -14,7 +14,7 @@ const AccountTemplate = () => {
       <AccountSidebar />
       <div className='h-[700px]'>
         <h1 className='text-red-10'>rge</h1>
-        <CustomToggleSwitch/>
+
       </div>
     </div>
   )
